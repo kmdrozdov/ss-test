fc5fab16ba1b408a86c9d35d3c6aace1010db5af
Kirill Drozdov <k.drozdov@adsterra.com>
2023-11-29 12:38:20 +0200

    storage access api

---
+++ storage-access.js
@@ -0,0 +1,39 @@
+function doThingsWithCookies() {
+  document.cookie = 'foo=bar';
+}
+
+async function handleCookieAccess() {
+  if (!document.hasStorageAccess) {
+    doThingsWithCookies()
+  } else {
+    const hasAccess = await document.hasStorageAccess();
+
+    if (hasAccess) {
+      doThingsWithCookies();
+    } else {
+      try {
+        const permission = await navigator.permissions.query({
+          name: 'storage-access',
+        });
+
+        if (permission.state === 'granted') {
+          await document.requestStorageAccess();
+          doThingsWithCookies();
+        } else if (permission.state === 'prompt') {
+          try {
+            await document.requestStorageAccess();
+            doThingsWithCookies()
+          } catch (err) {
+            console.log(`Error obtaining storage access: ${err}`);
+          }
+        } else if (permission.state === 'denied') {
+          // nothing we can do
+        }
+      } catch (e) {
+        console.log(`Could not access permission state. Error: ${e}`);
+      }
+    }
+  }
+}
+
+await handleCookieAccess();
